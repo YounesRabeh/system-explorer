7c33dd8649f8881d3dd4d47b0208bde5047aa318
Younes Rabeh <93033194+YounesRabeh@users.noreply.github.com>
2024-04-14 12:21:06 +0200

    FileManager.java update

---
+++ src/systemx/utils/FileManager.java
@@ -0,0 +1,340 @@
+package systemx.utils;
+
+import java.io.*;
+import java.util.List;
+
+import java.util.ArrayList;
+
+import systemx.exceptions.DoNotExistsException;
+
+
+/**
+ * A utility class for file related operations.
+ * <p>
+ * This class provides methods to work with an individual file.
+ *
+ * @author Younes Rabeh
+ * @version under development
+
+ */
+public final class FileManager {
+    private FileManager(){}
+
+
+    /**
+     * Reads the content of a file and returns it as a list of strings.
+     * <p>
+     * This method reads the content of a file and returns it as a list of strings.
+     * Each string in the list represents a line in the file.
+     *
+     * @param file The file to read
+     * @return a list of strings representing  content of the file
+     * @throws DoNotExistsException if the file does not exist
+     */
+    public static List<String> getFileLines(File file) throws DoNotExistsException {
+        List<String> lines = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+            String line;
+            while ((line = reader.readLine()) != null) lines.add(line);
+        } catch (Exception e){
+            throw new DoNotExistsException(file);
+        }
+
+        return lines;
+    }
+
+    /**
+     * Reads the content of a file and returns it as a list of strings.
+     * <p>
+     * This method reads the content of a file and returns it as a list of strings.
+     * Each string in the list represents a line in the file.
+     *
+     * @param file The file to read
+     * @param start The line number to start reading from
+     * @param end The line number to stop reading at
+     * @return a list of strings representing the content of the file
+     * @throws DoNotExistsException if the file does not exist
+     */
+    public static List<String> getFileLines(File file, Integer start, Integer end) throws DoNotExistsException {
+        List<String> lines = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+            int fileLines = countLines(file);
+            String line;
+            if (start < 0 || start > fileLines) throw new IndexOutOfBoundsException();
+            if (end < 0 || end > fileLines) throw new IndexOutOfBoundsException();
+            int lineNumber = 0;
+            while ((line = reader.readLine()) != null) {
+                lineNumber++;
+                if (lineNumber >= start && lineNumber <= end) lines.add(line);
+            }
+        } catch (Exception e){
+            throw new DoNotExistsException(file);
+        }
+
+        return lines;
+    }
+
+    /**
+     * Counts the number of lines in a file.
+     * <p>
+     * This method counts the number of lines in a file and returns the count.
+     *
+     * @param file The file to count the lines of
+     * @return The number of lines in the file
+     * @throws DoNotExistsException if the file does not exist
+     */
+    public static int countLines(File file) throws DoNotExistsException {
+        int count = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+            while (reader.readLine() != null) count++;
+        } catch (Exception e){
+            throw new DoNotExistsException(file);
+        }
+
+        return count;
+    }
+
+    /**
+     * Appends the content of a file to another file.
+     * <p>
+     * This method appends the content of a file to another file.
+     *
+     * @param file The file to append to
+     * @param fileToAppend The file to append
+     * @throws DoNotExistsException if the file to append does not exist
+     */
+    public static void appendToFile(File file, File fileToAppend) throws DoNotExistsException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+             BufferedReader reader = new BufferedReader(new FileReader(fileToAppend))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (Exception e) {
+            throw new DoNotExistsException(file);
+        }
+    }
+
+    /**
+     * Appends an array of strings to a file.
+     * <p>
+     * This method appends an array of strings to a file.
+     *
+     * @param file The file to append to
+     * @param lines The array of strings to append
+     * @throws DoNotExistsException if the file does not exist
+     */
+    public static void appendToFile(File file, String[] lines) throws DoNotExistsException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+            for (String line : lines) {
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (Exception e) {
+            throw new DoNotExistsException(file);
+        }
+    }
+
+    /**
+     * Appends a string to a file.
+     * <p>
+     * This method appends a string to a file.
+     *
+     * @param file The file to append to
+     * @param line The string to append
+     * @throws DoNotExistsException if the file does not exist
+     */
+    public static void appendToFile(File file, String line) throws DoNotExistsException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+            writer.write(line);
+            writer.newLine();
+        } catch (Exception e) {
+            throw new DoNotExistsException(file);
+        }
+    }
+
+    /**
+     * Overrides The content of a file with the content of another file.
+     * @param file The file to override
+     * @param newFile The file to override with
+     * @throws DoNotExistsException if the file does not exist
+     */
+    public static void overrideFile(File file, File newFile) throws DoNotExistsException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+             BufferedReader reader = new BufferedReader(new FileReader(newFile))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (Exception e) {
+            throw new DoNotExistsException(file);
+        }
+    }
+
+    /**
+     * Overrides the content of a file with an array of strings.
+     * @param file The file to override
+     * @param lines The array of strings to override with
+     * @throws DoNotExistsException if the file does not exist
+     */
+    public static void overrideFile(File file, String[] lines) throws DoNotExistsException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            for (String line : lines) {
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (Exception e) {
+            throw new DoNotExistsException(file);
+        }
+    }
+
+    /**
+     * Overrides the content of a file with a string.
+     * @param file The file to override
+     * @param lineNumber The line number to override
+     * @param newLine The string to override with
+     * @throws DoNotExistsException if the file does not exist
+     * @throws IndexOutOfBoundsException if the line number is out of bounds
+     */
+    public static void overrideLine(File file, Integer lineNumber, String newLine) throws DoNotExistsException {
+        List<String> lines = getFileLines(file);
+        if (lineNumber < 1 || lineNumber > lines.size()) throw new IndexOutOfBoundsException();
+        lines.set(lineNumber - 1, newLine);
+        overrideFile(file, lines.toArray(new String[0]));
+    }
+
+
+    /**
+     * Overrides a section of a file with an array of strings.
+     * @param file The file to override
+     * @param start The line number to start overriding from
+     * @param end The line number to stop overriding at
+     * @param newLines The array of strings to override with
+     * @throws DoNotExistsException if the file does not exist
+     * @throws IndexOutOfBoundsException if the line numbers are out of bounds
+     */
+    public static void overrideSection(File file, Integer start, Integer end, String[] newLines) throws DoNotExistsException {
+        List<String> lines = getFileLines(file);
+        if (start < 1 || start > lines.size()) throw new IndexOutOfBoundsException();
+        if (end < 1 || end > lines.size()) throw new IndexOutOfBoundsException();
+        for (int i = start - 1; i < end; i++) {
+            lines.set(i, newLines[i - start + 1]);
+        }
+        overrideFile(file, lines.toArray(new String[0]));
+    }
+
+    /**
+     * Overrides a section of a file with a file.
+     * @param file The file to override
+     * @param fileToInsert The file to override with
+     * @param lineNumber The line number to override
+     * @throws DoNotExistsException if the file does not exist
+     */
+    public static void insertFile(File file, File fileToInsert, Integer lineNumber) throws DoNotExistsException {
+        List<String> lines = getFileLines(file);
+        List<String> linesToInsert = getFileLines(fileToInsert);
+        if (lineNumber < 1 || lineNumber > lines.size()) throw new IndexOutOfBoundsException();
+        lines.addAll(lineNumber - 1, linesToInsert);
+        overrideFile(file, lines.toArray(new String[0]));
+    }
+
+    /**
+     * Inserts a string to a file.
+     * @param file The file to insert
+     * @param lines The array of strings to insert
+     * @param lineNumber The line number to insert at
+     * @throws DoNotExistsException if the file does not exist
+     * @throws IndexOutOfBoundsException if the line number is out of bounds
+     */
+    public static void insertLines(File file, String[] lines, Integer lineNumber) throws DoNotExistsException {
+        List<String> fileLines = getFileLines(file);
+        if (lineNumber < 1 || lineNumber > fileLines.size()) throw new IndexOutOfBoundsException();
+        for (int i = 0; i < lines.length; i++) {
+            fileLines.add(lineNumber - 1 + i, lines[i]);
+        }
+        overrideFile(file, fileLines.toArray(new String[0]));
+    }
+
+    /**
+     * Inserts a string to a file.
+     * @param file The file to insert
+     * @param line The string to insert
+     * @param lineNumber The line number to insert at
+     * @throws DoNotExistsException if the file does not exist
+     * @throws IndexOutOfBoundsException if the line number is out of bounds
+     */
+    public static void insertLine(File file, String line, Integer lineNumber) throws DoNotExistsException {
+        List<String> fileLines = getFileLines(file);
+        if (lineNumber < 1 || lineNumber > fileLines.size()) throw new IndexOutOfBoundsException();
+        fileLines.add(lineNumber - 1, line);
+        overrideFile(file, fileLines.toArray(new String[0]));
+    }
+
+    /**
+     * Deletes a line from a file.
+     * @param file The file to delete from
+     * @param start The line number to start deleting from
+     * @param end The line number to stop deleting at
+     * @throws DoNotExistsException if the file does not exist
+     * @throws IndexOutOfBoundsException if the line number is out of bounds
+     */
+    public static void deleteSection(File file, Integer start, Integer end) throws DoNotExistsException {
+        List<String> lines = getFileLines(file);
+        if (start < 1 || start > lines.size()) throw new IndexOutOfBoundsException();
+        if (end < 1 || end > lines.size()) throw new IndexOutOfBoundsException();
+        lines.subList(start - 1, end).clear();
+        overrideFile(file, lines.toArray(new String[0]));
+    }
+
+
+    /**
+     * Deletes a line from a file.
+     * @param file The file to delete from
+     * @param lineNumber The line number to delete
+     * @throws DoNotExistsException if the file does not exist
+     * @throws IndexOutOfBoundsException if the line number is out of bounds
+     */
+    public static void deleteLine(File file, Integer lineNumber) throws DoNotExistsException {
+        List<String> lines = getFileLines(file);
+        if (lineNumber < 1 || lineNumber > lines.size()) throw new IndexOutOfBoundsException();
+        lines.remove(lineNumber - 1);
+        overrideFile(file, lines.toArray(new String[0]));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        String fileName = "PathResolver.java";
+        List<String> fileLines = null;
+        try {
+            fileLines = getFileLines(new File(fileName));
+        } catch (DoNotExistsException e) {
+            System.out.println(e.getMessage());
+        }
+
+        if (fileLines == null) return;
+        for (String line : fileLines) {
+            System.out.println(line);
+        }
+
+
+    }
+
+
+
+
+}
